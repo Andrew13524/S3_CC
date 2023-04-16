5d4a0e643696bdae8de391e88d42e2fd1e42449b
Andrew <andrewdp56@outlook.com>
2023-04-16 14:33:02 -0400

    Initial Commit

---
+++ S3/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace S3
+{
+    class Card
+    {
+        public string Suit;
+        public string Value;
+
+        public Card()
+        {
+
+        }
+        public Card(string suit, string value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+    }
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Setup();
+
+            //Card card2 = new Card();
+            //Card card = new Card("Clubs", "King");
+
+            //int num = 1;
+            //int num2 = 3;
+
+            //int result = AddNum(num, num2);
+            //Console.WriteLine(result);
+
+            Tile tile = new Tile();
+            Tile tile2 = new Tile();
+
+            Tile[] array = new Tile[16];
+
+            Tile[,] tiles = new Tile[4, 4];
+
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    tiles[i, j] = new Tile();
+                }
+            }
+
+            tiles[0, 2].Value = 2;
+        }
+
+        
+        
+
+        /// <summary>
+        /// Adds to numbers together.
+        /// </summary>
+        /// <param name="a">First number to be added</param>
+        /// <param name="b">Second number to be added</param>
+        private static int AddNum(int a, int b)
+        {
+            return a + b;
+        }
+        private static int AddNum(int a)
+        {
+            return a + 1;
+        }
+
+        private static void Setup()
+        {
+            Console.WriteLine("-----------");
+            Console.WriteLine("| Welcome |");
+            Console.WriteLine("-----------");
+        }
+    }
+}
---
+++ S3/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S3
+{
+    class Tile
+    {
+        public int Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+        private int value;
+
+        public Tile()
+        {
+
+        }
+        public Tile(int value)
+        {
+            this.value = value;
+        }
+    }
+}
